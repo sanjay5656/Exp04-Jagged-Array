cad9c6eb927531181b411d7915a31881f2168ae9
sanjay5656 <sanjaysanjay23ai@gmail.com>
2024-04-22 14:13:25 +0530

    san

--- Program.cs
+++ Program.cs
@@ -3,6 +3,19 @@ public class Pattern
 {
     public static void Main()
     {
-      // type the code here
+      int[][] jaggedArray = new int[4][];
+      jaggedArray[0] = new int[3];
+      jaggedArray[1] = new int[5];
+      jaggedArray[2] = new int[6];
+      jaggedArray[3] = new int[4];
+      for (int i = 0; i < jaggedArray.Length; i++)
+      {
+        for (int j = 0; j < jaggedArray[i].Length; j++)
+        {
+          jaggedArray[i][j] = i * j + 60;
+          Console.WriteLine("CPU usage of {0} node is {1}%", i + 1, jaggedArray[i][j]);
+
+        }
+      }
     }
 }
--- obj/Debug/net6.0/Exp04-Jagged-Array.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exp04-Jagged-Array.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exp04-Jagged-Array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ce7d41fc3210c492e6ad16fd18f99771ba1e5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exp04-Jagged-Array")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exp04-Jagged-Array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
